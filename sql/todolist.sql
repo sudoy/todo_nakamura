ed25ad9d8a46d206d391bd6b901743454e079a12
N_F <N_F@DESKTOP-6RD84GH>
2018-05-29 13:35:26 +0900

    SQL修正（TABLE内の入力必須箇所の修正

--- sql/todolist.sql
+++ sql/todolist.sql
@@ -1,16 +1,16 @@
 CREATE TABLE todolist(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	title VARCHAR(100) NOT NULL,
-	detail TEXT,
+	detail TEXT NOT NULL,
 	priority INT NOT NULL,
-	limit_day DATE NOT NULL
+	limit_day DATE
 );
 
 INSERT INTO todolist 
-VALUES (1,'買い物', 'あ', 3, '2018/05/01');
+VALUES (1,'買い物', '新宿に14時', 3, '2018/05/01');
 INSERT INTO todolist 
-VALUES (2,'勉強', '', 2, '2018/05/02');
-INSERT INTO todolist 
-VALUES (3,'食事', '南青山のおいしいフレンチ', 1, '2018/05/03');
-INSERT INTO todolist(title, priority, limit_day)
-VALUES ('勉強', 2, '2018/05/04');
+VALUES (2,'勉強', '課題１～５をやる', 2, '2018/05/02');
+INSERT INTO todolist(title, detail, priority, limit_day)
+VALUES ('食事', '南青山のおいしいフレンチ', 1, '2018/05/03');
+INSERT INTO todolist(title, detail, priority)
+VALUES ('勉強（この前の復習をする）', '再復習しないとやばいかもしれない', 2);
